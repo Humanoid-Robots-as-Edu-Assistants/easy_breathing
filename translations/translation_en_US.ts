9e9c9d6d0adba45efd28a0a04ed17539ec4fa4fc
LeweC <lewe22lewe@googlemail.com>
2023-01-11 13:24:39 +0100

    initial commit

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Atmet entspannt ein!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Atmet entspannt ein!</translation>
+        </message>
+        <message>
+            <source>Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden und die Übung beginnt.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden und die Übung beginnt.</translation>
+        </message>
+        <message>
+            <source>Da ich schlecht im Zuhören bin und Emily nicht die ganze Zeit vergeblich Start sagen will, können meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Da ich schlecht im Zuhören bin und Emily nicht die ganze Zeit vergeblich Start sagen will, können meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </translation>
+        </message>
+        <message>
+            <source>Da ich schlecht im Zuhören bin, werden jetzt meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Da ich schlecht im Zuhören bin, werden jetzt meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </translation>
+        </message>
+        <message>
+            <source>Da ich schlecht im Zuhören bin, werden jetzt meine Fußknöpfe gedrückt, damit die Übung beginnt. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Da ich schlecht im Zuhören bin, werden jetzt meine Fußknöpfe gedrückt, damit die Übung beginnt. </translation>
+        </message>
+        <message>
+            <source>Da ich schlecht im Zuhören bin, müssen jetzt meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Da ich schlecht im Zuhören bin, müssen jetzt meine Fußknöpfe gedrückt werden, damit die Übung beginnt. </translation>
+        </message>
+        <message>
+            <source>Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung drei Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung drei Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hoffentlich sehen wir uns bald wieder! Ich wünsche euch noch eine schöne Woche... Tschüss!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hoffentlich sehen wir uns bald wieder! Ich wünsche euch noch eine schöne Woche... Tschüss!</translation>
+        </message>
+        <message>
+            <source>Ich würde mich freuen, euch bald wieder zu sehen! Ich wünsche euch noch eine schöne Woche... Tschüss!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ich würde mich freuen, euch bald wieder zu sehen! Ich wünsche euch noch eine schöne Woche... Tschüss!</translation>
+        </message>
+        <message>
+            <source>Tschüss!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Tschüss!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Tschüss, bis bald!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tschüss, bis bald!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein..</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein..</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung eine Minute lang.Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung eine Minute lang.Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein..</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein..</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung fünf Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung fünf Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein..</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein..</translation>
+        </message>
+    </context>
+</TS>
