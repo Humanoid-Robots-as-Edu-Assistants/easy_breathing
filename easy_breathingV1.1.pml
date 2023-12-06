16201643e4ac1a60042cc07ee5ea2c28ed5c3476
Communications Lab Berlin <122447611+FB1-Labor-KOM@users.noreply.github.com>
2023-12-06 12:16:25 +0100

    update v1.1

---
+++ easy_breathingV1.1.pml
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="easy_breathingV1.1" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="easyBreathingthree" src="easyBreathingthree.wav" />
+        <File name="easyBreathingone" src="easyBreathingone.wav" />
+        <File name="easyBreathingfive" src="easyBreathingfive.wav" />
+        <File name="Metal_Gong-Dianakc-109711828" src="Metal_Gong-Dianakc-109711828.mp3" />
+        <File name="zapsplat_bell_small_hand_short_ring_005_84224" src="zapsplat_bell_small_hand_short_ring_005_84224.mp3" />
+        <File name="icon" src="icon.png" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="de_DE">
+        <Translation name="translation_de_DE" src="translations/translation_de_DE.ts" language="de_DE" />
+    </Translations>
+</Package>
---
+++ translations/translation_de_DE.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Drei Minuten/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung drei Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung drei Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Drei Minuten/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein..</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein..</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Eine Minute/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung eine Minute lang.Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung eine Minute lang.Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Fünf Minuten/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Wir machen die Übung fünf Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Wir machen die Übung fünf Minuten lang. Wenn alle bereit sind, können meine Fußknöpfe gedrückt werden, um die Übung zu starten.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Fünf Minuten/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Alles klar! Atmet jetzt entspannt ein.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Alles klar! Atmet jetzt entspannt ein.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Outro/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Tschüss, bis bald!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Tschüss, bis bald!</translation>
+        </message>
+    </context>
+</TS>
